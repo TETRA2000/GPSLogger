4d68d215635922a4d9aee299f5f2b9b6619fb459
TETRA2000 <sagittariusm25@gmail.com>
2021-11-12 08:31:00 +0900

    init

---
+++ app/src/main/java/dev/tetra2000/gpslogger/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.tetra2000.gpslogger
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
