494361fcb0ed130767ade090a6485475fb74b2bd
TETRA2000 <sagittariusm25@gmail.com>
2021-11-13 12:18:00 +0900

    Add foreground service

---
+++ app/src/main/java/dev/tetra2000/gpslogger/LocationMonitorService.kt
@@ -0,0 +1,53 @@
+package dev.tetra2000.gpslogger
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.IBinder
+import androidx.annotation.RequiresApi
+
+class LocationMonitorService : Service() {
+    private val ONGOING_NOTIFICATION_ID = 1
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val channelId =
+            createNotificationChannel("my_service", "My Background Service")
+
+        val pendingIntent: PendingIntent =
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(this, 0, notificationIntent, 0)
+            }
+
+        val notification: Notification =
+            Notification.Builder(this, channelId)
+                .setContentTitle(getText(R.string.notification_title))
+                .setContentText(getText(R.string.notification_message))
+//            .setSmallIcon(R.drawable.icon)
+                .setContentIntent(pendingIntent)
+                .setTicker(getText(R.string.ticker_text))
+                .build()
+
+        // Notification ID cannot be 0.
+        startForeground(ONGOING_NOTIFICATION_ID, notification)
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    // https://stackoverflow.com/a/47533338
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String{
+        val chan = NotificationChannel(channelId,
+            channelName, NotificationManager.IMPORTANCE_NONE)
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
+    }
+}
--- app/src/main/java/dev/tetra2000/gpslogger/MainActivity.kt
+++ app/src/main/java/dev/tetra2000/gpslogger/MainActivity.kt
@@ -1,5 +1,6 @@
 package dev.tetra2000.gpslogger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startLocationMonitorService();
+    }
+
+
+    private fun startLocationMonitorService() {
+        Intent(this, LocationMonitorService::class.java).also { intent ->
+            applicationContext.startForegroundService(intent)
+        }
     }
 }
